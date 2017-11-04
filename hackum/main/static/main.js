6cd617351a9485e4724780edafc5915d001c1862
James Woglom <j@wogloms.net>
2017-11-04 16:21:53 -0400

    ui for welcome page

--- hackum/main/static/main.js
+++ hackum/main/static/main.js
@@ -1,3 +1,7 @@
 window.$ = document.querySelector.bind(document);
 window.$$ = document.querySelectorAll.bind(document);
 
+$("#start-button").onclick = function start() {
+	$("#welcome").style.display = 'none';
+	$("#setup").style.display = '';
+}
